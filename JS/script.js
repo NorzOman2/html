c819098766a6d74ac7f1c9d7272af8a2f243653a
NorzOman2 <arshaddj257@gmail.com>
2025-02-24 16:36:36 +0530

    Create JS/script.js: Basic Structure Setup

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,7 @@
 document.getElementById("actionBtn").addEventListener("click", function() {
     alert("Button clicked!");
 });
+
+```javascript
+// This JavaScript file is currently empty.  Add your JavaScript code here.
+```
